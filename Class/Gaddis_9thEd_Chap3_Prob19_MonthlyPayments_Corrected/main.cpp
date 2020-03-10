7cf9c01c34c270021b2e8f22fc3ffe2f06504efb
ml1150258 <mark.lehr@rcc.edu>
2020-03-10 11:57:38 -0700

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

--- Class/Gaddis_9thEd_Chap3_Prob19_MonthlyPayments_Corrected/main.cpp
+++ Class/Gaddis_9thEd_Chap3_Prob19_MonthlyPayments_Corrected/main.cpp
@@ -17,6 +17,8 @@ using namespace std;  //Libraries compiled under std
 //Conversions, Higher Dimensions
 const unsigned char CNVMNTH=12; //Number of months/year
 const unsigned char CNVPERC=100;//Percent to Decimal
+const unsigned char CNVPNYS=100;//Conversion from $'s to Pennies
+const float HLFAPNY=0.005f;//Half a Penny
 
 //Function Prototypes
 
@@ -35,16 +37,17 @@ int main(int argc, char** argv) {
     loanAmt=1e4f;//$10,000 
     numCmpd=36;  //Number of Compounding Periods
     
-    //Mapping Process Inputs to Outputs
+    //Mapping Process Inputs to Outputs, Knowns to Unknowns
     intRate/=static_cast<float>(CNVMNTH);//Convert %/month
     intRate/=static_cast<float>(CNVPERC);//Convert to Decimal
     float temp=pow(1+intRate,numCmpd);//Calc once use twice
     mnPymnt=intRate>0?//Monthly Payment blows up at 0% interest rate
             intRate*temp*loanAmt/(temp-1)://temp used twice
             loanAmt/numCmpd;//What if interest rate was equal to 0
+    int imnPymt=(mnPymnt+HLFAPNY)*CNVPNYS;//Discrete number of pennies
+    mnPymnt=static_cast<float>(imnPymt)/CNVPNYS;
     totPaid=mnPymnt*numCmpd;//Total Paid for Loan
     intPaid=totPaid-loanAmt;//Total interest paid for Loan
-    
     
     //Display Outputs
     cout<<fixed<<setprecision(2)<<showpoint;
@@ -52,6 +55,8 @@ int main(int argc, char** argv) {
     cout<<"Interest Rate:      "<<setw(5)
             <<static_cast<int>(intRate*CNVPERC)
             <<"%"<<endl;
+    cout<<"Number of Payments: "<<setw(5)
+            <<static_cast<int>(numCmpd)<<endl;
     cout<<"Monthly Payment:   $"<<setw(8)<<mnPymnt<<endl;
     cout<<"Interest Paid:     $"<<setw(8)<<intPaid<<endl;
     cout<<"Total Paid:        $"<<setw(8)<<totPaid<<endl;
